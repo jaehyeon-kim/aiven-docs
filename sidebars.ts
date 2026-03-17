bda1cddbc0fa0ac1545ca110fadd24bb951d3eee
dorota <114921900+wojcik-dorota@users.noreply.github.com>
2026-03-17 10:09:24 +0100

    feat: valkey multi-version and version 9 (#1310)
    Signed-off-by: dorota <114921900+wojcik-dorota@users.noreply.github.com>

--- sidebars.ts
+++ sidebars.ts
@@ -1979,6 +1979,7 @@ const sidebars: SidebarsConfig = {
                     'products/valkey/howto/estimate-max-number-of-connections',
                     'products/valkey/howto/manage-ssl-connectivity',
                     'products/valkey/howto/create-valkey-read-replica',
+                    'products/valkey/howto/valkey-version-upgrade',
                   ],
                 },
                 {
